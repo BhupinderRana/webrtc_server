0eed83e3865726c273499d1e81bf6a02524ecc7b
BhupinderRana <bhupinder.zimo@outlook.com>
2021-06-29 10:23:31 +0530

    server

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  await app.listen(3000, '192.168.18.50');
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+
+bootstrap().then();
